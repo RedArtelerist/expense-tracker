5fb8e571d589ab91ccec53a065e5c64e1b6ad8c7
Maksym Fedorenko <fedorenko.max02@gmail.com>
2024-05-01 02:25:21 +0300

    Fixed checkstyle

--- expense-service/src/main/java/com/redartis/expense/service/AccountService.java
+++ expense-service/src/main/java/com/redartis/expense/service/AccountService.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
